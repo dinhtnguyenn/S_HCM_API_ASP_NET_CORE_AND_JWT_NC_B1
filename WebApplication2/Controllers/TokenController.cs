976156c964b04cfd865cd63b3998de262371a445
Dinh Nguyen <32100818+dinhtnguyenn@users.noreply.github.com>
2021-09-07 08:47:34 +0700

    up

--- WebApplication2/Controllers/TokenController.cs
+++ WebApplication2/Controllers/TokenController.cs
@@ -84,7 +84,7 @@ namespace WebApplication2.Controllers
 
             var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"], _configuration["Jwt:Audience"], claims, expires: DateTime.Now.AddMinutes(1), signingCredentials: signIn);
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"], _configuration["Jwt:Audience"], claims, expires: DateTime.Now.AddDays(1), signingCredentials: signIn);
 
 
             return Ok(new Token(new JwtSecurityTokenHandler().WriteToken(token)));
